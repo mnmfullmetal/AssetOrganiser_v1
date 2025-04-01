af87f37512d38a72547ea86ba734f58de25877e6
myles gillard <mnm.fullmetal@gmail.com>
2025-04-01 21:12:54 +1100

    discovered EditorApplication.delaycall, a delegate that is called once after all inspectors update. I added MoveTexture as a delegate function to this to be called once when the inspectors update, effectively solving my previous issue where there was a timing mismatch causing discrepencies in the assetdatabase within unity.
    Currently only handles images being imported, and assumes that there is already a folder into which to place them.
    
    Next steps are to expand the asset types that the program targets and account for the event of no folder to move the assets to.

--- Assets/Editor/AssetImportOrganiser.cs
+++ Assets/Editor/AssetImportOrganiser.cs
@@ -6,18 +6,40 @@ using System.IO;
 
 public class AssetImportOrganiser : AssetPostprocessor
 {
+    // OnPostprocessTexture called when texture2D has finished importing into editor, but is not yet compressed. 
     private void OnPostprocessTexture(Texture2D texture)
     {
         Debug.Log("texture imported" + assetPath);
-        string newPath = "Assets/Images/" + Path.GetFileName(assetPath);
 
-        if (AssetDatabase.MoveAsset(assetPath, newPath) == "")
+        // EditorApplication.delaycall is a static event of EditorApplication class, I have used this to register a delegate function that will be called after the editor updates. 
+        EditorApplication.delayCall += () => MoveTexture(assetPath);
+
+    }
+
+    //Movetexture Function called when editor refreshes, after importation of assets
+    private void MoveTexture(string path)
+    {
+        string newPath = Path.Combine("Assets/Images/", Path.GetFileName(path));
+
+        if (path == newPath)
+        {
+            Debug.Log("Texture already in correct folder: " + newPath);
+           
+            EditorApplication.delayCall -= () => MoveTexture(path);
+            return; 
+        }
+
+        string moveResult = AssetDatabase.MoveAsset(path, newPath);
+
+        if (moveResult == "")
         {
             Debug.Log("texture moved to: " + newPath);
         }
         else
         {
-            Debug.Log("texture move failed.");
+            Debug.Log("texture move failed. Error: " + moveResult);
         }
+       
+        EditorApplication.delayCall -= () => MoveTexture(path);
     }
 }
